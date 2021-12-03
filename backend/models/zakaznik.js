a12c78d3d3607ff493104cc5378c9092210bbe6c
fcaplak <f.caplak@centrum.sk>
2021-12-03 23:53:01 +0100

    finalne upravy

--- backend/models/zakaznik.js
+++ backend/models/zakaznik.js
@@ -4,14 +4,8 @@ const {
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Zakaznik extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
       this.hasOne(models.Objednavka, { foreignKey: 'customerId'});
-      //models.Objednavka.hasOne(this);
     }
   };
   Zakaznik.init({
